7dc69edacef301c37a7dbdc1da7ed59fd8d81d8f
kanghowoo <tiger452003@gmail.com>
2025-07-01 22:20:07 +0900

    first commit

---
+++ src/main/java/com/myrealstore/MyRealStoreApplication.java
@@ -0,0 +1,13 @@
+package com.myrealstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRealStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRealStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myrealstore/MyRealStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myrealstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRealStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
