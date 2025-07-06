d9c580e115254ad5d84582cc83b1775d02f50fd3
kanghowoo <tiger452003@gmail.com>
2025-07-06 21:43:07 +0900

    feat: 포인트 데이터를 가지는 member entity 추가

---
+++ src/main/java/com/myrealstore/member/domain/Member.java
@@ -0,0 +1,30 @@
+package com.myrealstore.member.domain;
+
+import com.myrealstore.global.common.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "point", nullable = false)
+    private int point = 0;
+}
