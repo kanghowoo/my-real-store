77c6e8ca0a122070dd7f057ea86d43ca15fa8d59
kanghowoo <tiger452003@gmail.com>
2025-07-08 20:17:47 +0900

    chore: thymeleaf 의존성 및 configuratioinPropertiesScan 설정 추가

--- src/main/java/com/myrealstore/MyRealStoreApplication.java
+++ src/main/java/com/myrealstore/MyRealStoreApplication.java
@@ -2,13 +2,14 @@ package com.myrealstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class MyRealStoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyRealStoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MyRealStoreApplication.class, args);
+    }
 
 }
